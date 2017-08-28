62d19d3b7fbb4a3182541c1ca4211325ad22280e
Hannu Honka <hannu.honka@tieto.com>
2017-08-28 18:25:27 +0300

    base commit

---
+++ app/scripts/app.js
@@ -0,0 +1,20 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name dashboardDemoApp
+ * @description
+ * # dashboardDemoApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('dashboardDemoApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngSanitize',
+    'ngTouch'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name dashboardDemoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the dashboardDemoApp
+ */
+angular.module('dashboardDemoApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
